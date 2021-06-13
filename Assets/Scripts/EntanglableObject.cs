41919e2d864f84378ef938fd197ba5eb492c940c
FreehandBlock51 <daltoncarter7@outlook.com>
2021-06-13 10:40:16 -0700

    fixed objects

--- Assets/Scripts/EntanglableObject.cs
+++ Assets/Scripts/EntanglableObject.cs
@@ -120,7 +120,7 @@ public class EntanglableObject : ResettableMonoBehaviour
         }
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnCollisionStay2D(Collision2D collision)
     {
         if (collision.gameObject.GetComponent<Player>())
         {
